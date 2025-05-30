6b8bb4670b92ee7eb0b4ec643f09ec07df21f9f0
Paylak Navasardyan <paylak2002n@gmail.com>
2025-05-30 23:28:50 +0400

    Add endpoint to return registration status and clean up routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,24 +16,36 @@ type RegDataType = {
   email: string,
   area?: string | number,
   course: string
-}
+};
+
+type ReplyMessageType = { status: string };
 
 let lastRegData: RegDataType | null = null;
+let replyMessage: ReplyMessageType = { 'status' : '' };
 
-app.get('/', (req : Request, res : Response) => {
+app.get('/', (req: Request, res: Response) => {
   if (lastRegData) {
     res.send(lastRegData);
   } else res.send('There is no data')
 });
 
-app.post('/Registration', (req : Request, res : Response) => {
+app.post('/Registration', (req: Request, res: Response) => {
   const { name, email, area, course } = req.body as RegDataType;
-
+   
   if (name && email && course) {
     lastRegData = req.body
+    replyMessage = { 'status': '200' };
     console.log('New registration՝', lastRegData);
     res.status(200).send('Registration was succesful');
-  } else res.status(400).send('Fill in all fields!');
+  } else {
+    replyMessage = { 'status': '400'};
+    res.status(400).send('Something went wrong, please try again later!');
+  }       
+});
+
+app.get('/Registration', (req: Request, res: Response) => {
+  res.send(replyMessage);
+  console.log('Status message is`', replyMessage)
 });
 
 app.listen(PORT, () => {
